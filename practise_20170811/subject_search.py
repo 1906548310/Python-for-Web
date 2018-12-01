9efd61974cd1a8a42b043ad1e7a1f8e615eaf972
Mike <1906548310@qq.com>
2018-12-02 00:04:47 +0800

    update by mike

--- practise_20170811/subject_search.py
+++ practise_20170811/subject_search.py
@@ -6,12 +6,18 @@
 # 1.在豆瓣上搜寻评论数高、评分高的绝版书；
 from urllib.request import urlopen
 from urllib.error import HTTPError, URLError
+import requests
 from bs4 import BeautifulSoup
 import csv
-import requests
+
 csvFile = open("../subject_douban_search.csv", 'w+', newline='')
 writer = csv.writer(csvFile)
 writer.writerow(['书名', '作者名', '评分'])
+session = requests.Session()
+headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)"
+                        "AppleWebKit 537.36 (KHTML, like Gecko) Chrome",
+           "Accept":"text/html,application/xhtml+xml,application/xml;"
+                    "q=0.9,image/webp,*/*;q=0.8"}
 def getNames(ProductList):
     try:
         url_origin = "https://book.douban.com/subject/" + ProductList + "/"
@@ -32,6 +38,6 @@ def getNames(ProductList):
         return ['非图书详情页']
     return BookName, WriterName, Score, url_origin
 
-for y in range(1039100, 1039105):
+for y in range(10391051, 10391055):
     writer.writerow(getNames(str(y)))
     print(getNames(str(y)), y)
